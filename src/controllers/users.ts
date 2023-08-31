3ff49188429ded4872e66eef3a87b6c29dac558d
tchiteu <matheussantos.contato362@gmail.com>
2023-08-31 02:18:37 -0300

    feat(controllers/users): creating create method with properties validation

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,13 +1,32 @@
-import { Request, Response } from 'express'
-import { User } from '../entity/User'
-import { AppDataSource } from '../data-source'
+import { Request, Response } from 'express';
+import { User } from '../entity/User';
+import { database } from '../data-source';
 
-export const list = async (req: Request, res: Response) => {
-  try {
-    const users = await AppDataSource.manager.find(User)
+export const create = async (req: Request, res: Response) => {
+  const {
+    name, email, password, language,
+  } = req.body;
+  const user = new User(name, email, password, language);
 
-    console.log(users)
-  } catch (err) {
-    console.log(err)
+  // validate body fields/properties
+  const errors = await user.validateProperties();
+
+  if (errors.length > 0) {
+    res.status(400).json(errors[0]);
   }
+
+  const existingUser = await database.findOne(User, {
+    where: { email: user.email },
+  });
+
+  // check if email already exists
+  if (existingUser) {
+    res.status(409).json({
+      field: 'email',
+      message: 'email is already in use',
+    });
+  }
+
+  // insert user into database
+  database.save(User, user);
 };
