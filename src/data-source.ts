b4a905d09ccc66c868b1a1a4b3d26f677c227d1c
tchiteu <matheussantos.contato362@gmail.com>
2023-08-31 02:08:02 -0300

    refactor(data-source): applying eslint fixes and renaming the database module

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,9 +1,9 @@
-import 'reflect-metadata'
-import { DataSource } from 'typeorm'
-import { User } from './entity/User'
-import { Shedule } from './entity/Shedule'
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import { User } from './entity/User';
+import { Shedule } from './entity/Shedule';
 
-export const AppDataSource = new DataSource({
+const AppDataSource = new DataSource({
   type: 'sqlite',
   database: 'database.sqlite',
   synchronize: true,
@@ -11,6 +11,8 @@ export const AppDataSource = new DataSource({
   entities: [User, Shedule],
   migrations: [],
   subscribers: [],
-})
+});
 
 AppDataSource.initialize();
+
+export const database = AppDataSource.manager;
