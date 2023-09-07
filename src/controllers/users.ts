07ec97c547e8991af306cecfad6456ca65aef2d8
tchiteu <matheussantos.contato362@gmail.com>
2023-09-07 18:39:29 -0300

    feat(controllers/users): adding password encryption in the create method

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,18 +1,20 @@
 import { Request, Response } from 'express';
 import { User } from '../entity/User';
 import { database } from '../data-source';
+import { hashPassword } from '../services/auth';
 
 export const create = async (req: Request, res: Response) => {
   const {
-    name, email, password, language,
+    name, email, password, manager, language,
   } = req.body;
-  const user = new User(name, email, password, language);
+
+  const user = new User(name, email, password, manager, language);
 
   // validate body fields/properties
   const errors = await user.validateProperties();
 
   if (errors.length > 0) {
-    res.status(400).json(errors[0]);
+    return res.status(400).json(errors[0]);
   }
 
   const existingUser = await database.findOne(User, {
@@ -21,12 +23,17 @@ export const create = async (req: Request, res: Response) => {
 
   // check if email already exists
   if (existingUser) {
-    res.status(409).json({
+    return res.status(409).json({
       field: 'email',
       message: 'email is already in use',
     });
   }
 
+  const hash = await hashPassword(user.password);
+  user.password = hash;
+
   // insert user into database
-  database.save(User, user);
+  const createdUser = await database.save(User, user);
+
+  return createdUser;
 };
