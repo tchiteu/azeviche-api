f8d1ad8c88001efe6691a64eb309c43a91dff8d1
tchiteu <matheussantos.contato362@gmail.com>
2023-09-07 18:32:05 -0300

    feat(env): setting env. vars

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
-import * as express from 'express';
+import express from 'express';
+import dotenv from 'dotenv';
 import routes from './routes';
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 4000;
