60918deb6753c6db27fbf5f8a65caa1e2a4992e4
tchiteu <matheussantos.contato362@gmail.com>
2023-09-07 18:36:08 -0300

    feat(services/auth): creating validate and create JWT methods

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,4 +1,5 @@
 import { genSalt, hash, compare } from 'bcrypt';
+import { sign, verify } from 'jsonwebtoken';
 import { User } from '../entity/User';
 
 export const hashPassword = async (password: string) => {
@@ -15,3 +16,24 @@ export const comparePassword = async (password: string, passwordHash: string) =>
   return match;
 };
 
+export const createJWT = (user: User) => {
+  const payload = {
+    id: user.id,
+    manager: user.manager,
+  };
+
+  const options = {
+    expiresIn: '8h',
+  };
+
+  const secretKey = process.env.JWT_SECRET;
+
+  return sign(payload, secretKey, options);
+};
+
+export const validateJWT = (token: string) => {
+  const secretKey = process.env.JWT_SECRET;
+  const decoded = verify(token, secretKey);
+
+  return decoded;
+};
