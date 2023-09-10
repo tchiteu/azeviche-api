d074c14d8d601dcf7d5ccd067ac819f88b9a21be
tchiteu <matheussantos.contato362@gmail.com>
2023-09-09 23:54:16 -0300

    feat(app): applying cors on express server

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import routes from './routes';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+app.use(cors());
 
 app.use(express.json());
 app.use('/', routes);
