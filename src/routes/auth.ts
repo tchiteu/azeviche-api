8a5ba3face61ac8166b70290063bb091416defcc
tchiteu <matheussantos.contato362@gmail.com>
2023-11-19 03:01:38 -0300

    feat(routes/auth): adding refresh-token route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { signIn } from '../controllers/auth';
+import { generateNewToken, signIn } from '../controllers/auth';
 
 const router = Router();
 
 router.use('/signin', signIn);
+router.use('/refresh-token', generateNewToken);
 
 export default router;
