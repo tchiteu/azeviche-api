f160aab6c768b4cb219ecff5661524d20661bab2
tchiteu <matheussantos.contato362@gmail.com>
2023-11-19 02:56:06 -0300

    feat(services/auth): adjusting createJWT method to receive expires param

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,5 +1,5 @@
 import { genSalt, hash, compare } from 'bcrypt';
-import { sign, verify } from 'jsonwebtoken';
+import { sign } from 'jsonwebtoken';
 import { User } from '../entity/User';
 
 export const hashPassword = async (password: string) => {
@@ -16,24 +16,13 @@ export const comparePassword = async (password: string, passwordHash: string) =>
   return match;
 };
 
-export const createJWT = (user: User) => {
+export const createJWT = (user: User, expiresIn: string) => {
   const payload = {
     id: user.id,
     manager: user.manager,
   };
 
-  const options = {
-    expiresIn: '8h',
-  };
-
   const secretKey = process.env.JWT_SECRET;
 
-  return sign(payload, secretKey, options);
-};
-
-export const validateJWT = (token: string) => {
-  const secretKey = process.env.JWT_SECRET;
-  const decoded = verify(token, secretKey);
-
-  return decoded;
+  return sign(payload, secretKey, { expiresIn });
 };
