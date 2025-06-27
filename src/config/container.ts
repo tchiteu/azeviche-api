6f7dd3f8255f9a96d07c5a40e482372e98f3cd88
tchiteu <matheussantos.contato362@gmail.com>
2025-06-27 02:12:06 -0300

    feat(container): add dependency injection setup for services and controllers

---
+++ src/config/container.ts
@@ -0,0 +1,22 @@
+import 'reflect-metadata';
+import { container } from 'tsyringe';
+import { PrismaClient } from '@prisma/client';
+import { TokenService } from '../services/token.service';
+import { UserService } from '../services/user.service';
+import { UserController } from '../controllers/user.controller';
+import { AuthService } from '../services/auth.service';
+import { AuthController } from '../controllers/auth.controller';
+
+const prisma = new PrismaClient();
+
+container.register<PrismaClient>('PrismaClient', { useValue: prisma });
+container.register('accessSecret', { useValue: process.env.ACCESS_TOKEN_SECRET });
+container.register('refreshSecret', { useValue: process.env.REFRESH_TOKEN_SECRET });
+
+container.registerSingleton(TokenService);
+
+container.registerSingleton(AuthService);
+container.registerSingleton(AuthController);
+
+container.registerSingleton(UserService);
+container.registerSingleton(UserController);
