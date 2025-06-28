aed046b12f04c6bbd527709355b74606117eb1b6
tchiteu <matheussantos.contato362@gmail.com>
2025-06-28 04:29:10 -0300

    feat(index): add project router to the main application and reorder imports

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,9 @@ import './config/container';
 import express from 'express';
 import bodyParser from 'body-parser';
 import generalRouter from './routes/general.route';
-import userRouter from './routes/user.route';
 import authRouter from './routes/auth.route';
+import userRouter from './routes/user.route';
+import projectRouter from './routes/project.route';
 import { errorHandler } from './middlewares/error.middleware';
 
 const PORT = Number(process.env.PORT) || 3000;
@@ -14,8 +15,9 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use('/', generalRouter)
-app.use('/users', userRouter);
 app.use('/auth', authRouter);
+app.use('/users', userRouter);
+app.use('/projects', projectRouter);
 app.use(errorHandler);
 
 app.listen(PORT, () => {
